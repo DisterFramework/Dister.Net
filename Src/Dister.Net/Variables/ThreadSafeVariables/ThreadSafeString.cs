22125a52b0901843524094127b0e7b36a3c0834b
Tmacioso <tmacioso@gmail.com>
2019-12-16 17:38:08 +0100

    Add ThreadSafeString

---
+++ Src/Dister.Net/Variables/ThreadSafeVariables/ThreadSafeString.cs
@@ -0,0 +1,15 @@
+﻿namespace Dister.Net.Variables.ThreadSafeVariables
+{
+    public class ThreadSafeString : ThreadSafeVariable<string>
+    {
+        public ThreadSafeString(string value) : base(value)
+        {
+        }
+
+        public void Append(string value)
+            => Set(TypeValue + value);
+
+        public static implicit operator ThreadSafeString(string value)
+            => new ThreadSafeString(value);
+    }
+}
