35b75faf9750bd22c8443a649f012bd814c6207d
Tmacioso <tmacioso@gmail.com>
2019-12-16 17:35:38 +0100

    Add ThreadSafeBool

---
+++ Src/Dister.Net/Variables/ThreadSafeVariables/ThreadSafeBool.cs
@@ -0,0 +1,15 @@
+﻿namespace Dister.Net.Variables.ThreadSafeVariables
+{
+    public class ThreadSafeBool : ThreadSafeVariable<bool>
+    {
+        public ThreadSafeBool(bool value) : base(value)
+        {
+        }
+
+        public void Flip()
+            => Set(!TypeValue);
+
+        public static implicit operator ThreadSafeBool(bool value)
+            => new ThreadSafeBool(value);
+    }
+}
