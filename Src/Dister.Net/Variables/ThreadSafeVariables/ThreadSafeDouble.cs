5f84740a387e17a41ccaedf840989195ed676b17
Tmacioso <tmacioso@gmail.com>
2019-12-16 17:33:54 +0100

    Add ThreadSafeDouble

---
+++ Src/Dister.Net/Variables/ThreadSafeVariables/ThreadSafeDouble.cs
@@ -0,0 +1,17 @@
+﻿namespace Dister.Net.Variables.ThreadSafeVariables
+{
+    public class ThreadSafeDouble : ThreadSafeVariable<double>
+    {
+        public ThreadSafeDouble(double value) : base(value)
+        {
+        }
+
+        public void Add(double value)
+            => Set(TypeValue + value);
+        public void Sub(double value)
+            => Set(TypeValue - value);
+
+        public static implicit operator ThreadSafeDouble(double value)
+            => new ThreadSafeDouble(value);
+    }
+}
